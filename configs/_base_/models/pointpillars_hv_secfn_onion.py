3a45e50ced6fff2c4984f20528ebc34d94aeb797
Yijun Jiang <yijun1121@gmail.com>
2023-07-07 16:25:24 -0400

    upload onion scripts

---
+++ configs/_base_/models/pointpillars_hv_secfn_onion.py
@@ -0,0 +1,93 @@
+voxel_size = [0.2, 0.2, 0.2]  # adjust according to your dataset
+point_cloud_range = [-40, -44, 3, 40, 44, 83]  # adjust according to your dataset
+model = dict(
+    type='VoxelNet',
+    data_preprocessor=dict(
+        type='Det3DDataPreprocessor',
+        voxel=True,
+        voxel_layer=dict(
+            max_num_points=32,
+            point_cloud_range=point_cloud_range,
+            voxel_size=voxel_size,
+            max_voxels=(16000, 40000))),
+    voxel_encoder=dict(
+        type='PillarFeatureNet',
+        in_channels=4,
+        feat_channels=[64],
+        with_distance=False,
+        voxel_size=voxel_size,
+        point_cloud_range=point_cloud_range),
+    # the `output_shape` should be adjusted according to `point_cloud_range`
+    # and `voxel_size`
+    middle_encoder=dict(
+        type='PointPillarsScatter', in_channels=64, output_shape=[400, 440, 400]),
+    backbone=dict(
+        type='SECOND',
+        in_channels=64,
+        layer_nums=[3, 5, 5],
+        layer_strides=[2, 2, 2],
+        out_channels=[64, 128, 256]),
+    neck=dict(
+        type='SECONDFPN',
+        in_channels=[64, 128, 256],
+        upsample_strides=[1, 2, 4],
+        out_channels=[128, 128, 128]),
+    bbox_head=dict(
+        type='Anchor3DHead',
+        num_classes=3,
+        in_channels=384,
+        feat_channels=384,
+        use_direction_classifier=True,
+        assign_per_class=True,
+        # adjust the `ranges` and `sizes` according to your dataset
+        anchor_generator=dict(
+            type='AlignedAnchor3DRangeGenerator',
+            ranges=[
+                [-40, -44, 56.6, 40, 44, 56.6],
+                [-40, -44, 52.8, 40, 44, 52.8],
+            ],
+            sizes=[[14, 12.5, 11.5], [15.2, 16.2, 15.7]],
+            rotations=[0, 1.57],
+            reshape_out=False),
+        diff_rad_by_sin=True,
+        bbox_coder=dict(type='DeltaXYZWLHRBBoxCoder'),
+        loss_cls=dict(
+            type='mmdet.FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_bbox=dict(
+            type='mmdet.SmoothL1Loss', beta=1.0 / 9.0, loss_weight=2.0),
+        loss_dir=dict(
+            type='mmdet.CrossEntropyLoss', use_sigmoid=False,
+            loss_weight=0.2)),
+    # model training and testing settings
+    train_cfg=dict(
+        assigner=[
+            dict(  # for Pedestrian
+                type='Max3DIoUAssigner',
+                iou_calculator=dict(type='BboxOverlapsNearest3D'),
+                pos_iou_thr=0.5,
+                neg_iou_thr=0.35,
+                min_pos_iou=0.35,
+                ignore_iof_thr=-1),
+            dict(  # for Cyclist
+                type='Max3DIoUAssigner',
+                iou_calculator=dict(type='BboxOverlapsNearest3D'),
+                pos_iou_thr=0.5,
+                neg_iou_thr=0.35,
+                min_pos_iou=0.35,
+                ignore_iof_thr=-1),
+        ],
+        allowed_border=0,
+        pos_weight=-1,
+        debug=False),
+    test_cfg=dict(
+        use_rotate_nms=True,
+        nms_across_levels=False,
+        nms_thr=0.01,
+        score_thr=0.1,
+        min_bbox_size=0,
+        nms_pre=10,
+        max_num=2))
---
+++ mmdet3d/datasets/onion_dataset.py
@@ -0,0 +1,42 @@
+import mmengine
+import numpy as np
+
+from mmdet3d.registry import DATASETS
+from .det3d_dataset import Det3DDataset
+from mmdet3d.structures import LiDARInstance3DBoxes
+
+from mmengine.logging import print_log
+
+@DATASETS.register_module()
+class OnionDataset(Det3DDataset):
+
+    # replace with all the classes in customized pkl info file
+    METAINFO = {
+        'classes': ('root', 'stem')
+    }
+
+    def parse_ann_info(self, info):
+        """Process the `instances` in data info to `ann_info`.
+
+        Args:
+            info (dict): Data information of single data sample.
+
+        Returns:
+            dict: Annotation information consists of the following keys:
+
+                - gt_bboxes_3d (:obj:`LiDARInstance3DBoxes`):
+                  3D ground truth bboxes.
+                - gt_labels_3d (np.ndarray): Labels of ground truths.
+        """
+        ann_info = super().parse_ann_info(info)
+        if ann_info is None:
+            ann_info = dict()
+            # empty instance
+            ann_info['gt_bboxes_3d'] = np.zeros((0, 7), dtype=np.float32)
+            ann_info['gt_labels_3d'] = np.zeros(0, dtype=np.int64)
+
+        # filter the gt classes not used in training
+        ann_info = self._remove_dontcare(ann_info)
+        gt_bboxes_3d = LiDARInstance3DBoxes(ann_info['gt_bboxes_3d'],origin=[0.5,0.5,0.5])
+        ann_info['gt_bboxes_3d'] = gt_bboxes_3d
+        return ann_info
